1cb0d0bfa12ddea392e5e95a5e08ad39a4f3317a
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2021-11-02 14:31:10 +0530

    Add files via upload

---
+++ Even or Odd.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace Even or Odd
+{
+   public class main_class
+   {
+      static System.Random random_generator = new System.Random();
+      public static void Main(string[] args)
+      {
+         string raptor_prompt_variable_zzyz;
+         ?? a;
+      
+         raptor_prompt_variable_zzyz ="Enter a number";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         a= Double.Parse(Console.ReadLine());
+         if (A % 2==0)
+         {
+            Console.WriteLine("The number is EVEN");
+         }
+         else
+         {
+            Console.WriteLine("The number is ODD");
+         }
+      }
+   }
+}
