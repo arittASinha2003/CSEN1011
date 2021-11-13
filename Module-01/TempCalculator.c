67852a97da415302bce82f191286c2151ca4e345
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2021-11-14 00:58:01 +0530

    Delete Module-01 directory

--- Module-01/TempCalculator.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-main() {
-  int c, f;
-  printf("Enter temperature in Celsius:");
-  scanf("%d", &c);
-  f = (c*9/5)+32;
-  printf("Converted temperature in Fahrenheit is %d", f);
-}
-
