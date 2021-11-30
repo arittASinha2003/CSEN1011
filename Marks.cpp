9bb47b3d3d630c84d4a6f2bd5cfb7bbeab8fd6c3
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2021-11-30 14:14:24 +0530

    Add files via upload

---
+++ Marks.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main() {
+    int m1, m2, m3, m4, m5;
+    
+    printf("Enter Marks 1 : ");
+    scanf("%d", &m1);
+
+    printf("Enter Marks 2 : ");
+    scanf("%d", &m2);
+
+    printf("Enter Marks 3 : ");
+    scanf("%d", &m3);
+
+    printf("Enter Marks 4 : ");
+    scanf("%d", &m4);
+
+    printf("Enter Marks 5 : ");
+    scanf("%d", &m5);
+
+    int total = m1 + m2 + m3 + m4 + m5;
+    
+    printf("Total Marks = %d\n", total);
+    printf("Average Marks = %d\n", total/5);
+
+    return 0;
+}
