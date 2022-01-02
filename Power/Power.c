2776eaf8fb149736cbf9b0fe3dcbacd04742b69a
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-02 10:51:04 +0530

    Add files via upload

---
+++ Power/Power.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+ { 
+   int i,j;
+   printf("Enter the Base: ");
+   scanf("%ld",&i);
+   printf("Enter the Power: ");
+   scanf("%ld" ,&j),
+   printf("The Calculated Value: %f",pow(i,j));
+   
+ }
