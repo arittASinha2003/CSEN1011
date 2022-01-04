50c43c988b1f609a0ca0d57a5fd14581af6fbbb5
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-04 14:34:46 +0530

    Update CallByValue-Reference.c

--- Call By Value-Reference/CallByValue-Reference.c
+++ Call By Value-Reference/CallByValue-Reference.c
@@ -33,3 +33,28 @@ int main()
   printf("\nAfter Calling swap_ref Function ...\n a = %d, b = %d\n", a, b);
   return 0;
 }
+
+/* Output
+Enter the value of a: 15
+Enter the value of b: 5
+Before Swapping ...
+ a = 15, b = 5
+Inside Swap Function ...
+ x = 5, y = 15
+After Swapping ...
+ a = 15, b = 5
+After Calling swap_ref Function ...
+ a = 5, b = 15
+ ~/CSEN1011-3/Module-01 Programs$ ./a.out
+ 
+ Enter the value of a: 10
+Enter the value of b: 20
+Before Swapping ...
+ a = 10, b = 20
+Inside Swap Function ...
+ x = 20, y = 10
+After Swapping ...
+ a = 10, b = 20
+After Calling swap_ref Function ...
+ a = 20, b = 10
+ ~/CSEN1011-3/Module-01 Programs$ ./a.out
