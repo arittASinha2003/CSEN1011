8f768bd9c1a017def83b4e7872deeba4c3eb6581
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-04 13:49:09 +0530

    Update CaseChange.c

--- Case Change/CaseChange.c
+++ Case Change/CaseChange.c
@@ -1,3 +1,10 @@
+/* Algorithm
+1. Start
+2. Accept a character
+3. If the character is lowercase, its ASCII value will be in the range 97 to 122 and upper case will be in the range 65 to 90.
+04. For conversion from lower case to upper case, subtract 32 and for conversion from upper to lower case, add 32.
+04. Stop */
+
 #include<stdio.h>
 int main() {
 	char ch;
