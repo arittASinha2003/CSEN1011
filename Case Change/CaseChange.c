da827dab6a8019ac333c5501ae311de0c1ea955b
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-04 13:56:22 +0530

    Update CaseChange.c

--- Case Change/CaseChange.c
+++ Case Change/CaseChange.c
@@ -2,8 +2,8 @@
 1. Start
 2. Accept a character
 3. If the character is lowercase, its ASCII value will be in the range 97 to 122 and upper case will be in the range 65 to 90.
-04. For conversion from lower case to upper case, subtract 32 and for conversion from upper to lower case, add 32.
-04. Stop */
+4. For conversion from lower case to upper case, subtract 32 and for conversion from upper to lower case, add 32.
+5. Stop */
 
 #include<stdio.h>
 int main() {
