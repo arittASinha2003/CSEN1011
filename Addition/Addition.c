b069af1e6eaebf2da7b98fa36ed1370f5c9455ea
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-04 14:31:15 +0530

    Update Addition.c

--- Addition/Addition.c
+++ Addition/Addition.c
@@ -1,3 +1,11 @@
+/* Algorithm
+
+1. Start
+2. Accept the two numbers
+3. Perform addition of the two numbers
+4. Display the result
+5. Stop */
+
 #include<stdio.h>
 
 int main()
@@ -16,3 +24,19 @@ int main()
    
    return 0;
 }
+
+/* Output
+Enter 1st number : 7
+Enter 2nd number : 14
+
+The sum of 7 and 14 is 21
+~/CSEN1011-3/Module-01 Programs$ ./a.out
+
+Enter 1st number : 5
+Enter 2nd number : 15
+
+The sum of 5 and 15 is 20
+
+Learnings
+01. Sum
+*/
