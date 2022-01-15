f6b485d2113331e20b8bcde5ccde0a53402beee4
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-16 02:49:30 +0530

    Update Factorial.c

--- Factorial/Factorial.c
+++ Factorial/Factorial.c
@@ -7,9 +7,9 @@
 
 #include<stdio.h>
 int main(){
-	int num,i,f=1;
-  
-  //printf("Enter a number: ");
+	int num, i, f=1, sum=0;
+	
+	//printf("Enter a number: ");
 	scanf("%d",&num);
   
 	if(num>=0 && num<10)
@@ -18,15 +18,24 @@ int main(){
 		{
 			f=f*i;
 		}
-		printf("%d",f);
+		printf("%d\n",f);
+		
+		// For finding sum of the digits of the factorial
+		while (f > 0)
+		{
+		    sum += (f % 10);
+		    f /= 10;
+		}
+		printf("Sum of the digits is %d\n", sum);
+		//Till here
 	}
 	else if(num>=10)
 	{
-		printf("Too big a number");
+		printf("Too big a number\n");
 	}
 	else
 	{
-		printf("Negative Number");
+		printf("Negative Number\n");
 	}
 	return 0;
 }
@@ -34,6 +43,7 @@ int main(){
 /* Output:
 //Enter a number: 5
 120
+Sum of the digits is 3
 
 //Enter a number: 10
 Too big a number
