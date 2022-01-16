9f95b694d9ac0a1450885fd12ed3b2ea614669b8
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-16 14:06:16 +0530

    Create ElementSearchArray.c

---
+++ Graded Assessment - 8/ElementSearchArray.c
@@ -0,0 +1,23 @@
+// Write a C program which searches for an element in the array. If the element is found, it should print its index. Else, it should print "Element not found".
+
+#include<stdio.h>
+#define SIZE 5
+int main(void)
+{
+    int data_list[SIZE];
+    int element;  //Element to be searched for
+    for (int i = 0; i < SIZE; i++)
+        scanf("%d", &data_list[i]);
+    scanf("%d", &element);
+    //Complete the rest of the code
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (data_list[i] == element)
+        {
+            printf("%d", i);
+            return 0;
+        }
+    }
+    printf("Element not found");
+    return 0;
+}
