3265451c80417929a57dcd0031f2e2bbb722dbea
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-16 20:02:28 +0530

    Update PrimeComposite.c

--- Prime or Composite/PrimeComposite.c
+++ Prime or Composite/PrimeComposite.c
@@ -1,3 +1,14 @@
+/* Algorithm:
+
+1. Start
+2. Accept a positive integer "n" (Number > 2)
+3. Initialise i with value 2
+4. Divide the number with the counter value
+5. If the reminder is 0, display that the number is composite and stop
+6. Else, increment the counter and repeat from step 4 until counter value is less than "n"
+7. After completion of all the steps, display that the number "n" is prime
+8. Stop */
+
 #include<stdio.h>
 int main() {
 	int num, i;
@@ -15,3 +26,11 @@ int main() {
 	printf("Prime");
 	return 0;
 }
+
+/* Output:
+Enter a number: 61
+Prime
+
+Enter a number: 54
+Composite
+*/
