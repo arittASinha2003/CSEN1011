527e30743a24f406ed300ebb91fa5308cbe6c6f3
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-16 19:53:23 +0530

    Update Power.c

--- Power/Power.c
+++ Power/Power.c
@@ -1,12 +1,26 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<math.h>
 int main()
  { 
-   int i,j;
-   printf("Enter the Base: ");
-   scanf("%ld",&i);
-   printf("Enter the Power: ");
-   scanf("%ld" ,&j),
-   printf("The Calculated Value: %f",pow(i,j));
+   double i, j, result;
    
+   printf("Enter the Base: ");
+   scanf("%lf",&i);
+   printf("Enter the Power: ");
+   scanf("%lf" ,&j),
+   
+   result = pow(i, j);
+   
+   printf("%.1lf ^ %.1lf = %.2lf", i, j, result);
+   return 0;
  }
+
+/* Output:
+Enter the Base: 25
+Enter the Power: 2
+25.0 ^ 2.0 = 625.00
+
+Enter the Base: 52.4
+Enter the Power: 4.8
+52.4 ^ 4.8 = 178974096.02
+*/
