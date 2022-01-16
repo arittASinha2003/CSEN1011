ab83797e3d188d79703ec66426a18e0cc728904c
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-16 22:24:23 +0530

    Update Swapping.c

--- Swapping/Swapping.c
+++ Swapping/Swapping.c
@@ -1,15 +1,31 @@
 #include<stdio.h>
-int main() {
-	int a = 1, b = 2;
+int main()
+{
+	int a, b;
 	
-	printf("Before Swapping\n");
+	printf("Enter 1st value: ");
+	scanf("%d", &a);
+	
+	printf("Enter 2nd value: ");
+	scanf("%d", &b);
+	
+	printf("Before Swapping...\n");
     printf("a=%d  b=%d\n", a, b);
     
     a = a + b;
     b = a - b;
     a = a - b;
     
-    printf("After Swapping\n");
+    printf("After Swapping...\n");
     printf("a=%d  b=%d\n", a, b);   
     return 0;
 }
+
+/* Output:
+Enter 1st value: 24
+Enter 2nd value: 25
+Before Swapping...
+a=24  b=25
+After Swapping...
+a=25  b=24
+*/
