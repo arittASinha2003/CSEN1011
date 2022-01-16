daa6cf05468f61950de68c6a424a88d01ca79f7d
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-16 21:15:30 +0530

    Update NetSalary.c

--- Salary/NetSalary.c
+++ Salary/NetSalary.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
 int main()
 {
-	int basic,hra,da,pf,it,netsalary;
-	printf("Enter the Basic:\n");
-	scanf("%d",&basic);
-	printf("Enter the DA:\n");
-	scanf("%d",&da);
-	printf("Enter the HRA:\n");
-	scanf("%d",&hra);
-	printf("Enter the PF:\n");
-	scanf("%d",&pf);
-	printf("Enter the IT:\n");
-	scanf("%d" ,&it);
-	netsalary=(basic+hra+da)-(pf+it);
-    printf("\nCalculated Net Salary: %d\n",(basic+hra+da)-(pf+it));
-    
+	float basic, hra, ta, others, da, pf, it, net_salary;
+	
+	printf("Enter Basic Salary: $");
+	scanf("%f",&basic);
+	printf("Enter HRA: $");
+	scanf("%f",&hra);
+	printf("Enter TA: $");
+	scanf("%f",&ta);	
+	printf("Enter Others: $");
+	scanf("%f",&others);	
+	
+	da = (basic*12)/100;
+	pf = (basic*14)/100;
+	it = (basic*10)/100;
+	
+	net_salary = basic + hra + ta + others + da - (pf+it);
+	
+	printf("Net Salary: $%.2f\n", net_salary);
+	
+	return 0;
 }
+
+/* Output:
+Enter Basic Salary: $10500
+Enter HRA: $3000
+Enter TA: $2500
+Enter Others: $2000
+Net Salary: $16740.00
+
+Enter Basic Salary: $12500.5
+Enter HRA: $3260.5
+Enter TA: $2740.2
+Enter Others: $1870.4
+Net Salary: $18871.54
+*/
