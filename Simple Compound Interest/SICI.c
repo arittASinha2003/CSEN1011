4fb9aaf5e4b58d31fb58bf8f7a94937db1901695
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-16 21:50:46 +0530

    Update SICI.c

--- Simple Compound Interest/SICI.c
+++ Simple Compound Interest/SICI.c
@@ -1,5 +1,10 @@
+/* Algorithm
+1. Start
+2. Accept Principle (p), Rate of interest (r), Time period(t)
+3. Display Simple Interest by evaluating p * r * t / 100 and also Compound Interest by using p * (pow(1+r/100, t) - 1)
+4. Stop */
+
 #include<stdio.h>
-#include<conio.h>
 #include<math.h>
 
 int main()
@@ -18,7 +23,21 @@ int main()
 	 /* Calculating Compound Interest */
 	 ci = p * (pow(1+r/100, t) - 1);
 	
-	 printf("Simple Interest = %0.3f\n", si);
-	 printf("Compound Interest = %0.3f", ci);
+	 printf("Simple Interest = %0.2f\n", si);
+	 printf("Compound Interest = %0.2f", ci);
 	 return(0);
 }
+
+/* Output:
+Enter Principal Amount: 5000
+Enter Time Period: 10
+Enter Rate of Interest: 5
+Simple Interest = 2500.00
+Compound Interest = 3144.47
+
+Enter Principal Amount: 1000000
+Enter Time Period: 10
+Enter Rate of Interest: 8
+Simple Interest = 800000.00
+Compound Interest = 1158925.88
+*/
