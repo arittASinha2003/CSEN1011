4795c8f5686364ec322c0ce0a1e7231038714b72
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-01-19 12:01:52 +0530

    Update and rename Question2.c to StocksProfit.c

--- Summative Assessment - 2/StocksProfit.c
+++ Summative Assessment - 2/StocksProfit.c
@@ -64,3 +64,36 @@ int main()
     }
     printf("%d", q - r);
 }
+
+/* Output:
+6
+10 20 30 40 50 60
+40
+*/
+
+/* OTHER WAY
+# include <stdio.h>
+# define SIZE 10
+int main() {
+  int stock_prices[SIZE];
+  int n,max=0;
+  printf("Enter the number of stock prices: ");
+  scanf("%d", &n);
+  printf("The stock prices are ...\n");
+  for(int i=0; i<n; i++)
+    scanf("%d", &stock_prices[i]);
+    for(int i=0; i<n; i++)
+      for (int j=i; j<n; j++) 
+        if ((stock_prices[j] - stock_prices[i]) > max)
+          max = stock_prices[j] - stock_prices[i];
+        
+  printf("The maximum profit that is possible is %d\n", max);
+  return 0;
+}
+
+/* Output:
+Enter the number of stock prices: 6
+The stock prices are ...
+10 20 30 40 50 60
+The maximum profit that is possible is 50
+*/
